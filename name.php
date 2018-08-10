4cedd984f3cbaabdaf8a7ba53bf2ab52ae6f45e9
sanju <@sanjuamutha@gmail.com>
2018-08-10 10:39:41 +0530

    done with htmlform and portfolio

---
+++ name.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+<form contact me="/contact me.php">
+Name <input type="text" name="first name" value=""><br>
+<input type="contact me" value="contact me">
+<p><a href="https://contact me.html"></a></p>
+</form>
+<p>Click the "contact me" button and the form-data will be sent to a page on the server called "contact me.php"</p>
+</body>
+</html>
